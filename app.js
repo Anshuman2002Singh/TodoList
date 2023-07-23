6f1c14fec0bd3f93db8b8b78b43d56b10ca0c5e0
Anshu <anshul.ansh.as@gmail.com>
2023-07-24 00:07:49 +0530

    FOURTH COMMIT

--- app.js
+++ app.js
@@ -23,14 +23,24 @@ app.get("/work",function(req,res){
 })
 app.post("/work", function (req, res) {
     let item = req.body.newItem;
+
     workItems.push(item);
     res.redirect("/");
 
 })
 app.post("/", function (req, res) {
     let item = req.body.newItem;
+    if(req.body.list==="Work")
+    {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else
+    {
     items.push(item);
+   
     res.redirect("/");
+    }
 
 })
 app.listen(3000, function () {
